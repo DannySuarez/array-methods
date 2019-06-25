a826fe5ec294f4b7653f6615294ec236325010e6
DannySuarez <suarezd10@gmail.com>
2019-06-25 11:55:14 -0700

    finished reduce and findIndex

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { map, filterArray, findIndex } = require('./index');
+const { map, filterArray, findIndex, reduce } = require('./index');
 
 describe('function testing', () => {
   it('can take a callback function and double', () => {
@@ -24,4 +24,29 @@ describe('function testing', () => {
     const filtered = filterArray(arr, arr => arr % 2 === 0);
     expect(filtered).toEqual([2, 4, 6, 8]);
   });
+
+  it('can find index of value', () => {
+    const arr = ['dog', 'cat', 'blue'];
+    const index = findIndex(arr, cat => cat == 'cat');
+    expect(index).toEqual(1);
+  });
+
+  describe('reduce function', () => {
+    it('iterated through an array and invokes callback', () => {
+      const numbers = [1, 2, 3];
+      const callback = jest.fn();
+
+      reduce(numbers, callback, 0);
+
+      expect(callback).toHaveBeenCalledTimes(numbers.length);
+    });
+    it('iterates through an array and sets updates the acc with result of callback', () => {
+      const numbers = [1, 2, 3];
+
+      const sum = reduce(numbers, (acc, item) => acc + item, 0);
+      expect(sum).toEqual(6);
+    });
+  });
 });
+
+
